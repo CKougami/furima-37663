6bc34b66a58b4c84d25821aafab994178a223c04
CKougami <100983695+CKougami@users.noreply.github.com>
2022-05-24 21:00:13 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name, presence: true
+    validates :first_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' }
+
+  has_many :items
+  has_many :trading_dates
+  
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password              { 'abc123' }
+    password_confirmation { 'abc123' }
+    family_name           { Gimei.last.kanji }
+    first_name            { Gimei.first.kanji }
+    family_name_kana      { Gimei.last.katakana }
+    first_name_kana       { Gimei.first.katakana }
+    birth_day             { '1988-09-26'}
+  end
+end
