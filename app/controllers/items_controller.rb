75914866275be4d81b3d97f0e01d08456552a4eb
CKougami <100983695+CKougami@users.noreply.github.com>
2022-05-30 21:03:54 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item =Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new             # 保存できなければnewアクションへ戻る
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :item_name, :introduction, :category_id, :item_condition_id, :postage_payer_id, 
+      :shipping_area_id, :preparation_day_id, :price, :image).merge(user_id: current_user.id)
+    # カラムにはないが、imageを添付するので必要。
+    # ユーザーIDはdeviseで使えるmergeメソッドで紐づける。
   end
 end
